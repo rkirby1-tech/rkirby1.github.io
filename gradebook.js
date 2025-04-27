9677915e432b4b2970789b7189d3ec691f914821
rkirby1-tech <rkirby1@my.waketech.edu>
2025-04-27 19:29:31 -0400

    Add RaylenKirby_gradebook.html and gradebook.js

---
+++ gradebook.js
@@ -0,0 +1,17 @@
+// TODO: Fetch date from the PostgreSQL datebase (to be implemented later)
+function fetchGradeData() {
+    // This function will query the PostgreSQL database and return grade data
+    console.log("Fetching grade data...");
+}
+
+// TODO: Populate the table with grade data
+function populationGradebook(data) {
+    // This function will take the fetched grade data and populate the table 
+    console.log("Populationg gradebook with data:", data);
+}
+
+// TODO REMOVE THIS
+// Call the stubs to demonstrate the workflow
+const gradeData = fetchGradeData();
+poplulateGradebook(gradeDate);
+// END REMOVE
